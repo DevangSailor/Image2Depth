49ce94c95c1c6a41ece730cbb9a31e6c05f51a5a
lalit184 <lalitsrinivas1@gmail.com>
2019-03-14 17:43:52 +0530

    json file correction

--- Json.py
+++ Json.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import csv
+import cv2
 """
 the objective of this mmethod is to 
 convert all annotations insto their json format
@@ -12,8 +13,13 @@ for file in glob.glob("./AirSim/*/*/"+"airsim_rec.txt"):
 		f=file.replace("airsim_rec.txt","images/")
 		if ";" in row[-1]:
 			image,pfm=row[-1].split(";")
-			if os.path.isfile(f+image) and os.path.isfile(f+pfm):	
-				dicts[f+image]=f+pfm
+			if os.path.isfile(f+image) and os.path.isfile(f+pfm):
+				try:
+					im=cv2..imread(f+image,0)
+					if np.count_nonzero(im) < 0.15*1300*1680:
+						dicts[f+image]=f+pfm
+				except:
+					print("entry removed")		
 import json
 with open('ImageAndDepthFile.json', 'w') as fp:
     json.dump(dicts, fp)	
